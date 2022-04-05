b9126af4a4c55c368bf5c94c57cb19d7cbd4f1a2
gzsamme <03SZ41586918@campus.economicas.uba.ar>
2022-04-05 09:59:23 -0300

    ejercicio1 cambios
    resoluciones hasta el 5

---
+++ Ejercicio1guiaCAI/Ej1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1guiaCAI
+{
+    //6. Lea las teclas presionadas y finalice al presionar Shift+Ctrl+F.
+    internal class Ejercicio1
+    {
+        public void Ej1()
+        {
+            //1. Presente el mensaje “Hola mundo” al usuario, espere la presión de una tecla y finalice.
+            Console.WriteLine("Hola Mundo!");
+        }
+
+        public void Ej2()
+        {
+            //2. Solicite el ingreso de un texto cualquiera y lo presente nuevamente al usuario.
+            string Nombre;
+            Console.WriteLine("Ingrese texto por favor:");
+            Nombre = Console.ReadLine();
+            Console.WriteLine(Nombre);
+        }
+
+        public void Ej3()
+        {
+            //3. Lea las teclas presionadas y finalice al presionar la tecla “X”.
+            do
+            {
+            } while (Console.ReadKey().Key != ConsoleKey.X);
+
+        }
+        public void Ej4()
+        {
+            //4. Permita el ingreso de frases, finalizando cuando el usuario ingresa “fin”.
+            do
+            {
+            } while (Console.ReadLine() != "fin");
+        }
+
+        public void Ej5()
+        {
+            //5. Lea las teclas presionadas y finalice al presionar Ctrl+F(Ctrl y F al mismo tiempo).
+            do
+            {
+            } while (Console.ReadKey().Modifiers != ConsoleModifiers.Control & Console.ReadKey().Key != ConsoleKey.F);
+        }
+
+    }
+}
---
+++ Ejercicio1guiaCAI/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Ejercicio1guiaCAI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ejercicio1 EJ = new Ejercicio1();
+
+            //EJ.Ej1();
+            //EJ.Ej2();
+            //EJ.Ej3();
+            //EJ.Ej4();
+            EJ.Ej5();
+
+
+
+
+
+            //Esta linea la ponemos para que no se cierre la consola
+
+
+        }
+    }
+}
